52dea15e5ca4970c7975f0c70254d372ed305a8e
joenis <jjosephrivadeneira@uniguajira.edu.co>
2024-09-29 12:24:08 -0500

    first commit

---
+++ frontend/src/app/models/cliente.ts
@@ -0,0 +1,9 @@
+export interface ClienteI {
+    nombreCliente: string;
+    direccionCliente: string;
+    telefonoCliente: string;
+    correoCliente: string;
+    passwordCliente: string;
+    activo: boolean; 
+
+}
